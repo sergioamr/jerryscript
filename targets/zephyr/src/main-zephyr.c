d547f3b6c9ee7b878e24ce3c10258b92e6307f1b
Sergio Martinez <sergio.martinez.rodriguez@intel.com>
2016-07-14 12:58:16 +0100

    Testing having an uart at the same time as the USB
    Signed-off-by: Sergio Martinez <sergio.martinez.rodriguez@intel.com>

--- targets/zephyr/src/main-zephyr.c
+++ targets/zephyr/src/main-zephyr.c
@@ -16,6 +16,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <device.h>
+#include <uart.h>
 
 #include <zephyr.h>
 #include <misc/printk.h>
@@ -154,6 +156,51 @@ const struct shell_cmd commands[] =
   SHELL_COMMAND (NULL, NULL)
 };
 
+static volatile bool data_transmitted;
+static volatile bool data_arrived;
+static	char data_buf[64];
+
+static void interrupt_handler(struct device *dev)
+{
+	uart_irq_update(dev);
+
+	if (uart_irq_tx_ready(dev)) {
+		data_transmitted = true;
+	}
+
+	if (uart_irq_rx_ready(dev)) {
+		data_arrived = true;
+	}
+}
+
+static void write_data(struct device *dev, const char *buf, int len)
+{
+	uart_irq_tx_enable(dev);
+
+	data_transmitted = false;
+	uart_fifo_fill(dev, buf, len);
+	while (data_transmitted == false)
+		;
+
+	uart_irq_tx_disable(dev);
+}
+
+static void read_and_echo_data(struct device *dev, int *bytes_read)
+{
+	while (data_arrived == false)
+		;
+
+	data_arrived = false;
+
+	/* Read all data and echo it back */
+	while ((*bytes_read = uart_fifo_read(dev,
+		data_buf, sizeof(data_buf)))) {
+		write_data(dev, data_buf, *bytes_read);
+	}
+}
+
+static const char *banner1 = "Send characters to the UART device\r\n";
+static const char *banner2 = "Characters read:\r\n";
 
 void main (void)
 {
@@ -161,6 +208,55 @@ void main (void)
   jerry_init (JERRY_INIT_EMPTY);
   shell_register_app_cmd_handler (shell_cmd_handler);
   shell_init ("js> ", commands);
+
+  struct device *dev;
+  uint32_t baudrate, bytes_read, dtr = 0;
+  int ret;
+
+  dev = device_get_binding(CONFIG_CDC_ACM_PORT_NAME);
+  if (!dev) {
+	  PRINT("CDC ACM device not found\n");
+	  return;
+  }
+
+  PRINT("Wait for DTR\n");
+  while (1) {
+	  uart_line_ctrl_get(dev, LINE_CTRL_DTR, &dtr);
+	  if (dtr)
+		  break;
+  }
+  PRINT("DTR set, start test\n");
+
+  /* They are optional, we use them to test the interrupt endpoint */
+  ret = uart_line_ctrl_set(dev, LINE_CTRL_DCD, 1);
+  if (ret)
+	  PRINT("Failed to set DCD, ret code %d\n", ret);
+
+  ret = uart_line_ctrl_set(dev, LINE_CTRL_DSR, 1);
+  if (ret)
+	  PRINT("Failed to set DSR, ret code %d\n", ret);
+
+  /* Wait 1 sec for the host to do all settings */
+  sys_thread_busy_wait(1000000);
+
+  ret = uart_line_ctrl_get(dev, LINE_CTRL_BAUD_RATE, &baudrate);
+  if (ret)
+	  PRINT("Failed to get baudrate, ret code %d\n", ret);
+  else
+	  PRINT("Baudrate detected: %d\n", baudrate);
+
+  uart_irq_callback_set(dev, interrupt_handler);
+  write_data(dev, banner1, strlen(banner1));
+  write_data(dev, banner2, strlen(banner2));
+
+  /* Enable rx interrupts */
+  uart_irq_rx_enable(dev);
+
+  /* Echo the received data */
+  while (1) {
+	  read_and_echo_data(dev, &bytes_read);
+  }
+
   /* Don't call jerry_cleanup() here, as shell_init() returns after setting
      up background task to process shell input, and that task calls
      shell_cmd_handler(), etc. as callbacks. This processing happens in
